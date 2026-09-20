fa7dd3250da21cc6f19db5b760cb5175c1f93a02
garland-0 <garlandhamisi@gmail.com>
2026-09-20 11:26:04 +0300

    Remove viewBinding and buildConfig features
    Removed buildFeatures configuration from build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,3 @@
-buildFeatures {
-        viewBinding = true
-        buildConfig = true
-    }
 plugins {
     id("com.android.application") version "8.7.3" apply false
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
