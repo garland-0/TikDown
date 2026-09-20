df3a65a462e1a5cea77a24dbfd6ff15bb547ec26
garland-0 <garlandhamisi@gmail.com>
2026-09-20 10:38:00 +0300

    Enable viewBinding and buildConfig features

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,7 @@
+buildFeatures {
+        viewBinding = true
+        buildConfig = true
+    }
 plugins {
     id("com.android.application") version "8.7.3" apply false
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
